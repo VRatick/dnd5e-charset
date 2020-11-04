ed311e8a10dd554aaa7089e45a26dfa60c20ab0f
DESKTOP-RE9G340\Ratick <v.ihnatii@gmail.com>
2020-11-04 14:38:11 +0200

    ADD:004 Add Inventory functional

--- src/screens/Inventory.js
+++ src/screens/Inventory.js
@@ -1,12 +1,24 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { changeParams } from '../redux/actions/charset';
+import text from '../assets/text.json';
+import { TextArea } from 'react-native-ui-lib';
 
-function Inventory() {
+function Inventory(props) {
+  const [characterSet, setCharacterSet ] = useState(props.characterSet)
+
     return (
-      <View>
-        <Text>Inventory!</Text>
+      <View style={{
+        height: '100%',
+        borderWidth: 1,        
+        padding: 10,        
+      }}>
+        <Text>{text[0].character.items}</Text>
+        <TextArea placeholder="Write something.." value={characterSet.items} onChangeText={ (value) => {
+            setCharacterSet({...characterSet, items: value})          
+          }
+        }/>
       </View>
     );
   }
