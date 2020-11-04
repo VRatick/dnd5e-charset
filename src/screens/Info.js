2fdb49ad30532a63d0e4f1b620bcdffa333292c9
DESKTOP-RE9G340\Ratick <v.ihnatii@gmail.com>
2020-11-04 15:20:50 +0200

    ADD:006 Add Info functional

--- src/screens/Info.js
+++ src/screens/Info.js
@@ -1,13 +1,59 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import { changeParams } from '../redux/actions/charset';
+import text from '../assets/text.json';
+import textForArray from '../assets/textForArray.json';
+import { TextArea } from 'react-native-ui-lib';
 
-function Info() {
-    return (
-      <View>
-        <Text>Info!</Text>
+function Info(props) {
+  const [characterSet, setCharacterSet ] = useState(props.characterSet);
+  const info = [];
+  const description = [];
+  
+  textForArray.infoText.forEach( (item) => {
+    info.push(
+      <View key={item} style={{
+        height: 150,
+        borderWidth: 1,        
+        padding: 10,        
+      }}>
+        <Text>{text[0].character[item]}</Text>        
+        <TextArea
+          placeholder="Write something.."
+          
+          onChangeText={text => {            
+            setCharacterSet({...characterSet, [item]: text})               
+          }}           
+          value={characterSet.item}/> 
       </View>
+    )
+  })
+  textForArray.descriptionText.forEach( (item) => {
+    description.push(
+      <View key={item} style={{
+        height: 150,
+        borderWidth: 1,        
+        padding: 10,        
+      }}>
+        <Text>{text[0].character[item]}</Text>        
+        <TextArea
+          placeholder="Write something.."
+          
+          onChangeText={text => {            
+            setCharacterSet({...characterSet, [item]: text})                          
+          }}           
+          value={characterSet.item}/> 
+      </View>
+    )
+  })
+    return (
+      <ScrollView>
+        <View>          
+          {description}
+          {info}
+        </View>
+      </ScrollView>
     );
   }
 
