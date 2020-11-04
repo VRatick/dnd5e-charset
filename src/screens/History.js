695eb14c429f58599a236eb78cd50a6717e19e78
DESKTOP-RE9G340\Ratick <v.ihnatii@gmail.com>
2020-11-04 14:42:48 +0200

    ADD:004 Add History functional

--- src/screens/History.js
+++ src/screens/History.js
@@ -1,12 +1,24 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { changeParams } from '../redux/actions/charset';
+import text from '../assets/text.json';
+import { TextArea } from 'react-native-ui-lib';
 
-function History() {
+function History(props) {
+  const [characterSet, setCharacterSet ] = useState(props.characterSet)
+
     return (
-      <View>
-        <Text>History!</Text>
+      <View style={{
+        height: '100%',
+        borderWidth: 1,        
+        padding: 10,        
+      }}>
+        <Text>{text[0].character.history}</Text>
+        <TextArea placeholder="Write something.." value={characterSet.history} onChangeText={ (value) => {
+            setCharacterSet({...characterSet, history: value})          
+          }
+        }/>
       </View>
     );
   }
