36f4ced264bde4c9eaae9067a13a790f077dbaa1
DESKTOP-RE9G340\Ratick <v.ihnatii@gmail.com>
2020-11-04 15:29:52 +0200

    ADD:007 Add coins to Inventory

--- src/screens/Inventory.js
+++ src/screens/Inventory.js
@@ -1,25 +1,50 @@
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import { changeParams } from '../redux/actions/charset';
 import text from '../assets/text.json';
+import textForArray from '../assets/textForArray.json';
 import { TextArea } from 'react-native-ui-lib';
 
 function Inventory(props) {
   const [characterSet, setCharacterSet ] = useState(props.characterSet)
+  const coins = [];
 
-    return (
-      <View style={{
-        height: '100%',
+  textForArray.coinsText.forEach( (item) => {
+    coins.push(
+      <View key={item} style={{
+        height: 100,
         borderWidth: 1,        
         padding: 10,        
       }}>
-        <Text>{text[0].character.items}</Text>
-        <TextArea placeholder="Write something.." value={characterSet.items} onChangeText={ (value) => {
-            setCharacterSet({...characterSet, items: value})          
-          }
-        }/>
+        <Text>{text[0].character[item]}</Text>        
+        <TextArea
+          placeholder="Write something.."
+          
+          onChangeText={text => {            
+            setCharacterSet({...characterSet, [item]: text})               
+          }}           
+          value={characterSet.item}/> 
       </View>
+    )
+  })
+
+    return (
+      <ScrollView>
+        {coins}
+        <View style={{
+          height: 500,
+          borderWidth: 1,        
+          padding: 10,        
+        }}>
+          <Text>{text[0].character.items}</Text>
+          <TextArea placeholder="Write something.." value={characterSet.items} onChangeText={ (value) => {
+              setCharacterSet({...characterSet, items: value}) 
+              console.log(characterSet.items);         
+            }
+          }/>
+        </View>
+      </ScrollView>
     );
   }
 
